5d45736fa6bfdcb1d13c4bb39ae199f6043b7a99
SWINGSM89\hegyo <swingsm0608@gmail.com>
2024-07-23 00:04:14 +0900

    java의 정석 유튜브 공부 업로드

---
+++ ch03_clone/src/Ex3_11.java
@@ -0,0 +1,17 @@
+
+public class Ex3_11 {
+
+	public static void main(String[] args) {
+		double pi = 3.141592;
+
+		System.out.println(pi);
+		System.out.println(pi*1000);
+		System.out.println(Math.round(pi*1000));
+		System.out.println(Math.round(pi*1000)/1000);
+		System.out.println(Math.round(pi*1000)/1000.0);
+		System.out.println((double)Math.round(pi*1000)/1000);
+
+		// double shortPi = Math.round(pi*1000) / 1000.0;
+		// System.out.println(shortPi);
+	}
+}
