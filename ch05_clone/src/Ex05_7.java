e53c706e9cefb4e02eacb6e494bcbac7e5d90a16
SWINGSM89\hegyo <swingsm0608@gmail.com>
2024-07-26 02:43:52 +0900

    java의 정석 유튜브 공부 업로드 - 2024-07-26

---
+++ ch05_clone/src/Ex05_7.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Ex05_7 {
+
+	public static void main(String[] args) {
+		System.out.println("매개변수의 개수:"+args.length);
+		for(int i=0; i<args.length;i++) {
+			System.out.println("args["+i+"] = \""+args[i]+"\"");
+		}
+	}
+
+}
